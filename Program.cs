3114ac23704335a079abc7cfe7fbfab79a4a92bd
MediaStation <MediaStation@MEDIASTATION-PC>
2023-11-15 01:51:53 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace programming_1_exercise_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
